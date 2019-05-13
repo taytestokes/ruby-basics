04e661803f53fb8e13feb1f9b86804573f1a3fd0
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-13 15:20:24 -0600

    added the variables section

---
+++ variables.rb
@@ -0,0 +1,15 @@
+# Variables in Ruby
+    # In order to refer to objects we want to assign them a name (variable)
+    # You can give an object a name by using the "=" assignment operator
+    # A name on the left side of the operator is assigned to the object on the right side of the assignment operator
+    # The name is not the object itself, just a way that is used to reference the actual object
+    number = 1
+
+# Reusing variables
+    # names are unique in ruby so names can only be used for one object
+    # if you assign different values to a variable then the latest assignment will overwrite everything
+    number = 2
+        # the number name is now re-assigned to the value of 2 and is no longer 1
+    
+# Different kinds of variables
+    # There are different kind of variables
