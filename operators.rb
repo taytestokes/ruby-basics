b71e3bbecac9a4e93071a7f6264f7ae74e96e283
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-14 13:44:07 -0600

    finished conditionals overview

---
+++ operators.rb
@@ -0,0 +1,32 @@
+# Operators
+
+# Arithematical Opertaors
+    # these are mathimatical operators
+    
+    # + addition
+    # - subtraction
+    # * multiplication
+    # / division
+    # ** expenentiation
+    # % modulus
+
+    # However some of these can be used on string types
+    # this is because the String class have their own operators that work the same way
+
+# Logical Operators
+    # these are also more commonly known as booleon operators
+    # this is the idea of either true or false
+
+    # and &&
+    # or ||
+    # not !
+
+# Comparison Operators
+    # Ruby has a bunch of  built-in operators to compare values
+    # Comparison operators will return true or false
+
+    # == equal to
+    # < less than
+    # <= less than or greater than
+    # > greater thans
+    
