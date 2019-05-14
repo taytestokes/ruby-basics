00bb0bfcde33b057413501da91150ab5624044bb
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-13 18:01:16 -0600

    finished the true, false, nil section for data types

---
+++ data_types.rb
@@ -0,0 +1,56 @@
+# Built in Data Types
+    # Data types are the types of objects used to represent data such as numbers, text, and other values
+
+# Six different data types in Ruby
+    # Numbers
+    # Strings (text)
+    # True, False, Nil
+    # Symbols
+    # Arrays
+    # Hashes
+
+# Numbers
+    # Numbers are simply just numbers
+    number = 123
+    # Negative numbers are created by prepending a minus in front of the number
+    negative_number = -123
+    # Decimals are created by including the decimal
+    decimal_number = 1.23
+    # You can use underscores to seperate thousands places 
+    thousand_number = 1_234 # ==> 1,234
+
+# Strings
+    # A string in a programming language is a text
+    
+    # There are a few ways to create a string in ruby
+        # you can use single quotes
+        'this is a string'
+        # or you can double
+        "this is a string too"
+
+    # You can concatenate strings by using the addition operator
+    'Hello' + 'this' + 'is' + 'a' + 'string' # ==> 'Hellothisisastring'
+
+    # You can also use a multiplication operator to repeat a string
+    'Hi' * 3 # ==> 'HiHiHi
+
+    # Multiplying a string in ruby will repeat the string
+
+    # Strings have built in methods
+    "hello".uppercase # ==> "HELLO"
+
+    "hello".capitalize # ==> "HELLO"
+
+    "hello".length # ==> 5
+
+    "hello".reverse # ==> "olleh"
+
+
+# True, False, NiL
+    # These are booleon data types
+        # True means true
+
+        # False means false
+
+        # Nil represents an object of nothing
+            # Every method returns something so sometimes we need to say that something is nothing
