d69cab9f685a7b918fc2e400e3b1b349d57bd44c
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-14 11:44:32 -0600

    added an overview for instance variables, attribute readers, and attribute writers

--- classes.rb
+++ classes.rb
@@ -59,3 +59,63 @@
           number.to_f / other
         end
       end
+
+# Initializing Objects
+    # lets define a new class
+    class Person
+        #before we add any methods to the class, we want to give it initial data
+        def initalize(name)
+            # initialize is a special method in ruby and it will be used everytime you intansiate a new object from a class using the new keyword
+            # this will pass the arguments given to the new method onto the initalize class method ex: Person.new('Tayte')
+        end
+    end
+
+# Instance Variables
+    # An objects own knowledge
+    # An instance variable are variables that are local to the objects scope
+    # An object will keep it's instance variable around as long as it exists
+    class Person
+        def initalize(name)
+            @name = name
+        end
+    end
+
+# Attriubute Readers
+    # Asking for information
+    # Attribute readers are methods that output the evaluation of an instance variable
+    # We can read our attributes or instance variables by calling them
+    class Person
+        def initalize(name)
+            @name = name
+        end
+
+        def sayName
+            @name
+        end
+    end
+
+    person = Person.new('Tayte')
+    puts person.sayname # ==> will output 'Tayte
+
+# Attribute Writers
+    # Sets information
+    # Attribute writers are methods that will set instance variables
+    class Person
+        def initalize(name)
+            @name = name
+        end
+
+        def sayName
+            @name
+        end
+        # this will set the instance variable of password
+        def password=(password)
+            @password = password
+        end
+    end
+
+    person = Person.new("Tayte")
+    person.password=("super secret")
+    # we could also write this like
+    person.password = "super secret" 
+    # this is because we are using the = operator in  our attribute write like so password=
