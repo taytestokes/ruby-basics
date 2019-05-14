487d736aac00d6500ca6aaa3db3d1df3e3d122da
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-13 18:25:32 -0600

    finished the arrays and symbols sections in data types

--- data_types.rb
+++ data_types.rb
@@ -54,3 +54,41 @@
 
         # Nil represents an object of nothing
             # Every method returns something so sometimes we need to say that something is nothing
+
+# Symbols
+    # Symbols are like string except they're code
+     
+    # A symbol is written likes this :something (a word preceeded by a colon)
+        # Symbols can not contain spaces so you would use underscores to replace the space
+
+# Arrays
+    # Arrays are containers for data in ruby
+    # Arrays are objects that store other objects
+    a_string = [1,2,3,4,5]
+
+    # Retrieving data from an array
+    words = ["one", "two", "three"]
+    puts words[1] # ==> "two"
+
+    # Arrays are zero based index, meaning the element at index of 1 is the second element in the array
+
+    # Appending to an Array
+        # use the shovel operator
+        words << "four" # ==> this is will push the word "four" onto the end of an array
+
+    # Things you can do with Arrays
+        # Add arrays
+        [1,2] + [3,4] # ==> [1,2,3,4]
+
+        # Subtract from each other
+        [:one, :two, :three] - [:three] # ==? [:one, :two]
+
+        # Multiply Arrays
+        [1,2] * 3 # ==> [1,2,1,2,1,2]
+
+        # Find the intersection
+        [1,2,3] & [2,3,4] # ==> [2,3]
+
+        # First and last elements can be pulled from the array
+        [1,2,3].first # ==> 1
+        [1,2,3].last # ==> 3
