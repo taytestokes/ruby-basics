e0869d104a629174e4fb1a61833699ebb16dc203
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-13 22:22:44 -0600

    added the classes overview

---
+++ classes.rb
@@ -0,0 +1,7 @@
+#Classes in ruby
+    # Classes create objects
+    # Objects are instances of classes
+
+    # Classes haves a bunch of characteristics, but most importantly, every class defines a set of methods that are specific to the tupe of object created
+    # Everytime an object is instantiated from a class, the object inherits all of the methods that are specific to the methods
+    # Objects inheirt methods from their classes
